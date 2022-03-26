5626b9174c8603f53e2e4fa472f0d32fca671e78
elenacalcan26 <elena.calcan26@gmail.com>
2022-03-26 02:05:44 +0200

    open + close

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ build: libso_stdio.so
 libso_stdio.so: so_stdio.o
 	$(CC) -shared $^ -o $@
 
-so_stdio.0: so_stdio.c
+so_stdio.o: so_stdio.c
 	$(CC) -Wall -Wextra -fPIC -c $^ -o $@
 
 clean:
--- so_stdio.c
+++ so_stdio.c
@@ -7,38 +7,117 @@
 
 #include "so_stdio.h"
 
-#define BUFF_LEN
+#define BUFF_LEN 4096
 
-typedef struct _so_file {
+struct _so_file {
     int fd;
     char *buffer;
     int currsor;
     int feof_flag;
     int ferror_flag;
 
-} SO_FILE;
+};
 
-// SO_FILE *so_fopen(const char *pathname, const char *mode) {
-//     SO_FILE *so_file;
-//     int fd;
+SO_FILE *so_fopen(const char *pathname, const char *mode) {
+    SO_FILE *so_file;
+    int fd = -1;
 
-//     if (strcmp(mode, "r") == 0) {
-//         fd = open(pathname, O_RDONLY, 0644);
-//     } else if (strcmp(mode, "r+") == 0) {
-//         fd = open(pathname, O_RDWR, 0644);
-//     } else if (strcmp(mode, "w") == 0) {
-//         fd = open(pathname, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-//     } else if (strcmp(mode, "w+") == 0) {
-//         fd = open(pathname, O_RDWR | O_CREAT | O_TRUNC, 0644);
-//     } else if (strcmp(mode, "a") == 0) {
-//         fd = open(pathname, O_APPEND | O_WRONLY | O_CREAT, 0644);
-//     } else if (strcmp(mode, "a+") == 0) {
-//         fd = open(pathname, O_APPEND | O_RDONLY | O_CREAT, 0644);
-//     }
+    if (strcmp(mode, "r") == 0) {
+        fd = open(pathname, O_RDONLY, 0644);
+    } else if (strcmp(mode, "r+") == 0) {
+        fd = open(pathname, O_RDWR, 0644);
+    } else if (strcmp(mode, "w") == 0) {
+        fd = open(pathname, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    } else if (strcmp(mode, "w+") == 0) {
+        fd = open(pathname, O_RDWR | O_CREAT | O_TRUNC, 0644);
+    } else if (strcmp(mode, "a") == 0) {
+        fd = open(pathname, O_APPEND | O_WRONLY | O_CREAT, 0644);
+    } else if (strcmp(mode, "a+") == 0) {
+        fd = open(pathname, O_APPEND | O_RDONLY | O_CREAT, 0644);
+    }
 
-//     if (fd < 0) {
-//         exit(-1);
-//     }
+    if (fd < 0) {
+        return NULL;
+    }
 
-//     return so_file;
-// }
+    so_file = (SO_FILE *)malloc(sizeof(SO_FILE));
+
+    if (so_file == NULL) {
+        return NULL;
+    }
+
+    so_file->buffer = (char*)calloc(BUFF_LEN, sizeof(char));
+
+    if (so_file->buffer == NULL) {
+        return NULL;
+    }
+
+    so_file->fd = fd;
+    so_file->currsor = 0;
+    so_file->feof_flag = 0;
+    so_file->ferror_flag = 0;
+
+    return so_file;
+}
+
+int so_fileno(SO_FILE *stream) {
+    return 0;
+}
+
+int so_fflush(SO_FILE *stream) {
+    return 0;
+}
+
+int so_fseek(SO_FILE *stream, long offset, int whence) {
+    return 0;
+}
+
+long so_ftell(SO_FILE *stream) {
+    return 0;
+}
+
+size_t so_fread(void *ptr, size_t size, size_t nmemb, SO_FILE *stream) {
+    return 0;
+}
+
+size_t so_fwrite(const void *ptr, size_t size, size_t nmemb, SO_FILE *stream) {
+    return 0;
+}
+
+int so_fgetc(SO_FILE *stream) {
+    return 0;
+}
+
+int so_fputc(int c, SO_FILE *stream) {
+    return 0;
+}
+
+int so_feof(SO_FILE *stream) {
+    return stream->feof_flag;
+}
+
+int so_ferror(SO_FILE *stream) {
+    return stream->ferror_flag;
+}
+
+SO_FILE *so_popen(const char *command, const char *type) {
+    return 0;
+}
+
+int so_pclose(SO_FILE *stream) {
+    return 0;
+}
+
+int so_fclose(SO_FILE *stream) {
+    int ret;
+
+    ret = close(stream->fd);
+    free(stream->buffer);
+    free(stream);
+
+    if (ret < 0) {
+        return SO_EOF;
+    }
+
+    return 0;
+}
